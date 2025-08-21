3ab65d857a9048441ba2c1d578fb6742953835a7
TIZY275 <isabelloehme@web.de>
2025-08-21 19:28:52 +0200

    fixed navbar agb und dsgvo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -6,20 +6,32 @@ import AGB from "./pages/AGB";
 import Datenschutz from "./pages/Datenschutz";
 import "./App.css";
 
+function AppLayout() {
+  const location = useLocation();
+
+  // Definiere die Seiten, auf denen die Navbar NICHT angezeigt werden soll
+  const hideNavbarRoutes = ["/agb", "/datenschutz"];
+  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname.toLowerCase());
+
+  return (
+    <div className="app">
+      {!shouldHideNavbar && <Nav />}
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/agb" element={<AGB />} />
+          <Route path="/datenschutz" element={<Datenschutz />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Nav />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/agb" element={<AGB />} />
-            <Route path="/datenschutz" element={<Datenschutz />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppLayout />
     </Router>
   );
 }
